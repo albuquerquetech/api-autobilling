5732d3e92089c7549bc61b9cd019b92688349c52
albuquerquetech <167625492+albuquerquetech@users.noreply.github.com>
2024-04-20 15:32:01 -0300

    Starting the project

---
+++ src/main/java/apiautobilling/ApiAutobillingApplication.java
@@ -0,0 +1,13 @@
+package apiautobilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutobillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutobillingApplication.class, args);
+	}
+
+}
